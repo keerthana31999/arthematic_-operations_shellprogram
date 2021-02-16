94020356c1852a76c1bd8d689574097298ad12cb
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 17:28:48 +0530

    second operation

--- arthematic.sh
+++ arthematic.sh
@@ -3,3 +3,5 @@ read -p "enter the 3 intiger input with space" a b c
 	echo "all 3 input=" $a $b $c
 	result1=$(($a+$b*$c))
 	echo "result1="$result1
+	 result2=$(($a*$b+$c))
+        echo "result2="$result2
