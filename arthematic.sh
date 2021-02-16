5c001dd49ce65b057664f1833582f8337e69a08b
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 17:38:51 +0530

    Added::third operation

--- arthematic.sh
+++ arthematic.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -x
 read -p "enter the 3 intiger input with space" a b c
 	echo "all 3 input=" $a $b $c
+
 	result1=$(($a+$b*$c))
 	echo "result1="$result1
+
 	 result2=$(($a*$b+$c))
         echo "result2="$result2
+
+	 result3=$(($c+$a/$b))
+        echo "result3="$result3
